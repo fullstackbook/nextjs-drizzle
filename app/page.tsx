60c30019ea3e29fa792cdd254599d2985bd787f7
Travis Luong <travis@travisluong.com>
2023-12-21 18:15:38 +0800

    nextjs, drizzle orm, and vercel integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
-  return <div>Home</div>;
+import { getUsers } from "@/db/db";
+
+export default async function Home() {
+  const data = await getUsers();
+
+  return <div>{JSON.stringify(data)}</div>;
 }
---
+++ db/db.ts
@@ -0,0 +1,18 @@
+import "@/config";
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { sql } from "@vercel/postgres";
+import { users } from "./schema";
+
+export const db = drizzle(sql);
+
+export const getUsers = async () => {
+  const selectResult = await db.select().from(users);
+  console.log("Results", selectResult);
+  return selectResult;
+};
+
+export type NewUser = typeof users.$inferInsert;
+
+export const insertUser = async (user: NewUser) => {
+  return db.insert(users).values(user).returning();
+};
